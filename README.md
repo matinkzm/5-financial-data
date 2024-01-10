7764e2fa53482083ce0995dccafe501c11a6adf9
matinkzm <kazemi99.itsu@gmail.com>
2024-01-10 10:47:21 +0330

    final code

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 5-financial-data
+
+this is 5th project from this link: https://medium.com/@luisprooc/30-data-engineering-project-ideas-9ecf0a70cbea
+
+I found the csv data file from this link: https://data.world/data-society/used-cars-data
+its a dataset about used cars for sale.
+
+In this ETL project: First i created a dataframe from the csv file , Second change some of the columns names to make it cleaner and mor meaningful , then i removed 3 columns because they always had one value (this help the dataset to become a little lighter) , then i wrote line 32 and 35 to find some of the highest and the lowest peices and removed cars that have price lower than 200 dollars(or euros).
+
+At last for transform part I removed cars with registration year lower than 1900 (the year that cars created) and cars with registration year moer than 2024 (this year)(it makes the dataset lighter and cleaner)
+
+then load it into a local database.
+
+Extract = download and convert the csv file into a dataframe
+Transform = change column names , remove redundant rows and columns
+Load = load the data into a local datbase
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# this is 5th project from this link https://medium.com/@luisprooc/30-data-engineering-project-ideas-9ecf0a70cbea
+# in this project I created an ETL data pipeline for an autos data csv file
 import pandas as pd
-import numpy as np
+import sqlite3
 
+# convert the csv file into  a dataframe  for more process
 autos = pd.read_csv("autos.csv", encoding='Latin-1')
 
+# get the columns names
 custom_column_names = autos.columns
 custom_column_names = custom_column_names.tolist()
 
@@ -18,10 +22,10 @@ for i in range(0, len(custom_column_names)):
         custom_column_names[i] = "ad_created"
     else:
         continue
-
+# assign new columns names
 autos.columns = custom_column_names
 
-# remove columns thar always have only one value
+# remove columns that always have only one value
 autos.drop(["seller", "offerType", "nrOfPictures"], axis=1, inplace=True)
 
 # this is for finding some of the lowest prices
@@ -32,12 +36,20 @@ autos.drop(["seller", "offerType", "nrOfPictures"], axis=1, inplace=True)
 
 # remove cars with price lower than 200 dollars
 autos = autos[(autos["price"] > 200)]
-# print(autos.value_counts("price").sort_index(ascending=True).to_string())
+# next line is for checking that cars with price lower than 200 have been removed(uncomment to see)
+# print(autos.value_counts("price").sort_index(ascending=True).head(1000).to_string())
 
 
-# remove false data to make the set better
+# remove false dates to make the dataset better
+# for example it has registration year 1000 (before even cars created) and 9999 (in far future)
 for index in autos.index:
     if autos["registration_year"][index] < 1900 or autos["registration_year"][index] > 2024:
         autos.drop(index, inplace=True)
 
 # print(autos["registration_year"].sort_values(ascending=False).head(100))
+
+# create a local db and save the transformed data into it
+connection = sqlite3.connect("final_data.db")
+cursor = connection.cursor()
+# db name , which database to connect , replace if the database was existed , use dataframe index
+autos.to_sql("lighter_autos_data", connection, if_exists='replace', index=False)
